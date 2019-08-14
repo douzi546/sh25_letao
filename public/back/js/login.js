88bcaa5b48a394ce1c8184ffe81686233c1fabe5
douzi546 <806822474@qq.com>
2019-08-14 20:09:12 +0800

    登陆静态页-表单验证完成-后台验证完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -19,6 +19,9 @@ $(function(){
                         min: 2,
                         max: 6,
                         message: '用户名长度必须在2到6位'
+                    },
+                    callback:{
+                        message:'用户名不存在'
                     }
                 }
             },
@@ -31,11 +34,48 @@ $(function(){
                         min: 6,
                         max: 12,
                         message: '密码长度必须在6到12位'
+                    },
+                    callback:{
+                        message:'密码错误'
                     }
                 }
 
             }
 
         }
+    })
+
+
+    $("#form").on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑
+
+        $.ajax({
+            url:"/employee/employeeLogin",
+            type:"post",
+            dataType:"json",
+            data:$("#form").serialize(),
+            success:function(info){
+                console.log(info);
+                if(info.success){
+                    location.href = "index.html"
+                }
+
+                if(info.error == 1000){
+                    $("#form").data('bootstrapValidator').updateStatus("username","INVALID",'callback')
+
+                }
+                if(info.error == 1001){
+                    $("#form").data('bootstrapValidator').updateStatus("password","INVALID",'callback')
+                }
+
+            }
+
+        })
+    });
+
+
+    $("[type=reset]").on("click",function(){
+        $("#form").data('bootstrapValidator').resetForm();
     })
 })

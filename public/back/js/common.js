24c1709269a593a0c32c873e70d0478a991e0bef
douzi546 <806822474@qq.com>
2019-08-15 19:12:57 +0800

    首页静态完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -11,3 +11,51 @@ $( document ).ajaxStop(function() {
     },500)
 
 });
+
+if(location.href.indexOf("login.html") === -1){
+    $.ajax({
+        type:"get",
+        url:"/employee/checkRootLogin",
+        dataType:"json",
+        success:function(info){
+            console.log(info);
+            if(info.error === 400){
+                location.href = "login.html"
+            }
+        }
+    })
+
+}
+$(function(){
+    $(".category").click(function(){
+        $(".child").stop().slideToggle();
+    });
+
+
+    $(".icon_menu").click(function(){
+        $(".lt_aside").toggleClass("hidemenu");
+        $(".lt_main").toggleClass("hidemenu");
+        $(".lt_topbar").toggleClass("hidemenu");
+
+    });
+
+
+    $(".icon_logout").click(function(){
+        $("#myModal").modal('show');
+    });
+
+    $(".btn-logout").click(function(){
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            dataType:"json",
+            success:function(info){
+                if(info.success){
+                    location.href = "login.html"
+                }
+            }
+        })
+    })
+
+
+})

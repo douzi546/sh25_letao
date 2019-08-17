6ccc6b826de4d2fe96c52d05d51cfac8e0ec4dee
douzi546 <806822474@qq.com>
2019-08-17 21:08:44 +0800

    移动端静态首页完成、

---
+++ public/front/js/common.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2019/8/17.
+ */
+mui('.mui-scroll-wrapper').scroll({
+    deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    bounce: true,//是否启用回弹
+    indicators: false,
+});
+
+
+var gallery = mui('.mui-slider');
+gallery.slider({
+    interval:5000,//自动轮播周期，若为0则不自动播放，默认为0；
+});
